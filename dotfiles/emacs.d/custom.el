59639bce6994658ff8076ae91baae67d9bfc2948
Jonas Oberschweiber <jonas@tatooine.fritz.box>
2010-07-25 21:59:07 +0200

    Added vimrc

---
+++ dotfiles/emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "#1e1e27" :foreground "#cfbfad" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 110 :width ultra-condensed :family "Inconsolata"))))
+ '(mumamo-background-chunk-submode ((((class color) (min-colors 88) (background dark)) nil))))
